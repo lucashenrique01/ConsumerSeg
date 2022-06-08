dea43e25d45c110aa65d9b77ee731e7ddb943981
lucashenrique01 <lucas.paula@bandtec.com.br>
2022-06-08 07:11:58 -0300

    generate Dockerfile

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -11,19 +11,16 @@ namespace ConsumerSeg.Services
             dataEvent = DataEventConverter.jsonToDataEvent(eventt.data.ToString());
             Boolean responseProposal = true; //aqui seria para retornar de uma chamada a algum end point se para aquele id de client seria "valido" enviar uma proposta de seguro.            
             if(responseProposal){
-                requestData(dataEvent.idCliente,dataEvent.idContrato,dataEvent);
+                requestData(dataEvent.idCliente,dataEvent.idContrato);
             }else {
                 Console.Write("Cliente não elegivel a receber uma proposta de seguro");
             }
         }
 
-        public async Task requestData(string idCliente, string idContrato, DataEvent eventt){            
-                var stringTask = client.GetStringAsync("http://localhost:8080/teste");
+        public async Task requestData(string idCliente, string idContrato){            
+                var stringTask = client.GetStringAsync($"http://localhost:8080/financial/{idCliente}/{idContrato}");
                 var msg = await stringTask;
                 Console.Write(msg);
-                Console.Write(idCliente + idContrato);
-                Console.Write("PASSOU");
-                
         } 
     }
 }
